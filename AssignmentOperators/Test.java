903d0a0817570dbe41b2e9da182c2b7296d3a0fa
Rashini1998 <gamalathrashini@gmail.com>
2024-01-18 14:11:05 +0530

    Assignment Operators

---
+++ AssignmentOperators/Test.java
@@ -0,0 +1,20 @@
+package AssignmentOperators;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 10;
+        x+=5;
+        //x=x+1;
+        // x++; //post increment
+        // System.out.println("x = "+x);
+        // ++x; //pre increment
+        // System.out.println("x = "+x);
+
+       // int y=x++;
+       int y = ++x;
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+
+        
+    }
+}
