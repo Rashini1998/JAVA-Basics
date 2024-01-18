e7f65050622fbfd8168f9317f14efb7058ef59aa
Rashini1998 <gamalathrashini@gmail.com>
2024-01-18 11:32:14 +0530

    Mathematical Operators

---
+++ MathOperators/Test.java
@@ -0,0 +1,12 @@
+package MathOperators;
+
+public class Test {
+        public static void main(String[] args) {
+            int x,y,answer;
+            x=10;
+            y=30;
+            answer=x%y;
+            System.out.println(answer);
+        }
+    
+}
